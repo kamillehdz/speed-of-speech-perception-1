edb636fd6ecd7652f1f68a9db6d7c7b3f894277a
kamillehdz <kamillehdz@gmail.com>
2025-06-16 09:53:12 -0400

    server.js

--- server.js
+++ server.js
@@ -6,17 +6,18 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(express.json());
 
-app.post('/submit',(req, res) => {
-    const data = req.body;
-    const log = `${new Date().toISOString()} - Trial: ${data.trial}, Speed: ${data.playbackSpeed}\n`;
+app.post('/submit', (req, res) => {
+  const data = req.body;
+  const log = `${new Date().toISOString()} - Participant: ${data.participantId}, Trial: ${data.trial}, Speed: ${data.playbackSpeed}, Time: ${data.videoTime}\n`;
 
-    fs.appendFile('speed-data.txt', log, (err) => {
-        if (err) {
-            console.error('Error saving data;', err);
-            return res.status(500).send('Failed to save data');
-        }
-        res.sendStatus(200);
-    });
+  fs.appendFile('speed-data.txt', log, (err) => {
+    if (err) {
+      console.error('Error saving data:', err);
+      return res.status(500).send('Failed to save data');
+    }
+    console.log('Saved:', log);
+    res.sendStatus(200);
+  });
 });
 
 app.listen(port, () => {
